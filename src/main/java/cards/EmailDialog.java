afcac35269e3a9802634e6f6300e5322d1b0e7ba
Muhab Abubaker <mohababubakir2001@gmail.com>
2021-12-05 17:28:30 +0300

    adding emailDialog

---
+++ src/main/java/cards/EmailDialog.java
@@ -0,0 +1,29 @@
+package cards;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+public class EmailDialog {
+
+    @FXML
+    private Button cancelRankings;
+
+    @FXML
+    private Button confirmRankings;
+
+    @FXML
+    private ScrollPane ranksContainer;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmRanking(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/cards/EmailSlot.java
@@ -0,0 +1,19 @@
+package cards;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class EmailSlot {
+
+    @FXML
+    private Button emailBtn;
+
+    @FXML
+    private Label rankValue;
+
+    @FXML
+    private VBox studentContainer;
+
+}

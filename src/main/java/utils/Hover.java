c256239ddcb4aced5195573c09e6a3d3c93da16f
AbdulazizYas <abdulazizyass@gmail.com>
2021-12-15 18:22:29 +0300

    (Inshallah now will be added) Add Hover transition to Competition and Team cards. Using the created class Hover.java

---
+++ src/main/java/utils/Hover.java
@@ -0,0 +1,35 @@
+package utils;
+
+import javafx.animation.TranslateTransition;
+import javafx.scene.Node;
+import javafx.scene.effect.DropShadow;
+import javafx.util.Duration;
+import utils.transitions.ShadowTransition;
+
+public class Hover {
+
+    public static void raising(Node node){
+        TranslateTransition trans = new TranslateTransition(Duration.millis(200), node);
+
+        DropShadow drop = new DropShadow();
+        drop.setRadius(30);
+
+        ShadowTransition shadow = new ShadowTransition(Duration.millis(200), node, drop);
+        shadow.setRaised(true);
+
+        node.setOnMouseEntered(e -> {
+            trans.setByY(-3);
+            trans.playFromStart();
+
+            shadow.setIsIn(true);
+            shadow.playFromStart();
+        });
+        node.setOnMouseExited(e -> {
+            trans.setByY(3);
+            trans.playFromStart();
+
+            shadow.setIsIn(false);
+            shadow.playFromStart();
+        });
+    }
+}
